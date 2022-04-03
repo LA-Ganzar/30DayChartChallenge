c054fde7fc08c1db1ad4d48cb024e58fb5aa9de2
Leigh Ann Ganzar, DrPH MPH <88208717+LA-Ganzar@users.noreply.github.com>
2022-04-03 15:41:23 -0500

    Update Day2_Pictogram.R

--- Day 2/Day2_Pictogram.R
+++ Day 2/Day2_Pictogram.R
@@ -5,25 +5,22 @@
 # Data Source: USA Cycling
 
 library(tidyverse)
-library(showtext)
-library(usefunc)
-library(emojifont)
 library(waffle)
-library(extrafont)
 
 # write data
-df <- data.frame(group = c("Not specified", "Female", "Male"),
-                 value = c(5, 20, 75))
-
-# Waffle plot
-ggplot(df, aes(fill = group, values = value)) +
-  
-
-
-ggsave("/Users/leighannganzar/Desktop/Post-Doc/30DayChartChallenge/30DayChartChallenge/Day 2/plot.jpg", width = 10, height = 6)
-
 x <- c("Not Specified" = 5,  "Female" = 20, "Male" = 75)
 
 # Waffle chart
-waffle(x, rows = 10,
-       legend_pos = "bottom")
+waffle(x, rows = 10, xlab="1 square = 1 percent",
+       legend_pos = "bottom",
+       colors = c("#d4a012", "#d60b2f", "#031e41")) + 
+  labs(
+    title="Gender Distribution of 2022 USA Cycling Race Membership", 
+    caption="Source: USA Cycling | Graphic by Leigh Ann Ganzar") + 
+  theme(
+    plot.title = element_text(size = 14, face = "bold"),
+    axis.title = element_text(size = 10),
+    axis.title.y = element_blank())
+
+ggsave("/Users/leighannganzar/Desktop/Post-Doc/30DayChartChallenge/30DayChartChallenge/Day 2/plot.jpg", width = 10, height = 6)
+
